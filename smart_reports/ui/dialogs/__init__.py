180a7205159e16baa697ea31cc225c86863fdaca
davidverace-lab <davidvera12cc@gmail.com>
2025-11-03 10:41:47 -0600

    Delete smart_reports directory

--- smart_reports/ui/dialogs/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Diálogos de la aplicación
-"""
-from smart_reports.ui.dialogs.user_management_dialog import UserManagementDialog
-
-__all__ = ['UserManagementDialog']
--- smart_reports/ui/panels/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Panels package - Paneles modernos de la aplicación
-"""
-from smart_reports.ui.panels.modern_dashboard import ModernDashboard
-
-__all__ = ['ModernDashboard']
